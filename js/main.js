8c2cd3956872245c247b630d19ff64aad762ace1
cynthialu42 <clu2334@gmail.com>
2018-03-16 17:24:02 -0500

    minor timer cleanup logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     // variables
-    var intervalID = 0;
+    var intervalId = 0;
     var time = 0;
     let questions = [
         {
@@ -52,26 +52,28 @@ $(document).ready(function(){
     // start game will start first countdown asking to get ready
     // then call function to actually show the first question/answer pair
     let startTime = 0;
-    let beginGame = true;
+   
 
     let qNum = 0;
   
 
     // get first element of questions array
     function getQuestion(){
-        if (questions.length !== 0){
-            
-            let q = questions.shift(); // object 
+        // Check array is populated
+        if (questions.length > 0){
+            let q = questions.shift(); // take out the first object 
             $('.js-questions').text(q.question);
+            // print out answer selections
             for (var i = 0; i < q.answer.length; i++){
                 $('.js-answers').append('<input type = "radio" value = "' + q.answer[i] + '"name = "' + qNum + '">' + q.answer[i] + '</input');
             }
             
             //qNum++;
     
+            // More time in the actual questions timer
             let testTimer = true;
             let gameTime = 5;
-            setInterval(function(){
+            intervalId = setInterval(function(){
                 if (testTimer){
                     $('.js-time').text(gameTime);
                     gameTime--;
@@ -79,8 +81,7 @@ $(document).ready(function(){
                         testTimer = false;
                         console.log("time up for question");
                         $('.js-time').empty();
-                        // in between countdown
-                        
+                        // go to the inbetween  countdown
                         inBetweenCount();
                         // also show right answer and image
                         showAnswer();
@@ -99,9 +100,16 @@ $(document).ready(function(){
     function showAnswer(){
         console.log("showAnswer");
     }
+
+    function stopTimer(){
+        clearInterval(intervalId);
+    }
     function inBetweenCount(){
+        // get the user answer and compare it to the correct one
+        // new function isAnswerCorrect to check
         userAnswers = $('input[name='+qNum+']:checked').val();
-            console.log(userAnswers);
+        console.log(userAnswers);
+
         if( questions.length !== 0){
             let betweenCount = 3;
             let testTimer = true;
@@ -125,30 +133,24 @@ $(document).ready(function(){
             qNum++;
         }
         else{
-            console.log("done");
+            console.log("inbetween done");
         }
     }
     function startGame(){
-        
-        /*for (var i = 0; i < questions.length; i++){
-            $('.js-questions').append('<p>' + questions[i].question + '</p>');
-            for (var j = 0; j < questions[i].answer.length; j++){
-                $('.js-questions').append('<input type="radio" value = "'+questions[i].answer[j]+'" name = "'+ i + '">' + questions[i].answer[j]+'</input>');
-            }
-        }*/
+        let beginGame = true;
+        // Initialize get ready screen and preliminary countdown
         $('.js-questions').text("Get ready");
         //$('.js-title').addClass('hide');
         $('.js-start').addClass('hide');
         startTime = 3;
         intervalId = setInterval(function(){
-            
             if (beginGame){
                 $('.js-time').text(startTime);
                 startTime--;
                 if (startTime < 0){
                     beginGame = false;
                     // call the function to get first question
-                    console.log(startTime);
+                    console.log("Game Start");
                     $('.js-time').empty();
                     getQuestion();
                 }
@@ -168,3 +170,13 @@ $(document).ready(function(){
         stopGame();
     });
 });
+
+
+
+    
+        /*for (var i = 0; i < questions.length; i++){
+            $('.js-questions').append('<p>' + questions[i].question + '</p>');
+            for (var j = 0; j < questions[i].answer.length; j++){
+                $('.js-questions').append('<input type="radio" value = "'+questions[i].answer[j]+'" name = "'+ i + '">' + questions[i].answer[j]+'</input>');
+            }
+        }*/
